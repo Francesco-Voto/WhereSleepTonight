904ea2e08b3bb597cd97c149673d154f69f12eb4
Francesco <francesco.voto.8@gmail.com>
2022-12-18 22:30:29 +0100

    feat: create stack navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import {StatusBar} from 'react-native';
 import {QueryClientProvider} from 'react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 import queryClient from '@wst/services/queryClient';
 import theme, {ThemeProvider} from './theme';
 import Hotels from './Hotels';
+
+export type StackParamList = {
+  Hotels: undefined;
+};
+
+const Stack = createNativeStackNavigator<StackParamList>();
+const screenOptions = {
+  headerShown: false,
+};
 
 const App = () => {
   return (
@@ -12,7 +23,11 @@ const App = () => {
         <QueryClientProvider client={queryClient}>
           <>
             <StatusBar barStyle={'dark-content'} />
-            <Hotels />
+            <NavigationContainer>
+              <Stack.Navigator screenOptions={screenOptions}>
+                <Stack.Screen name="Hotels" component={Hotels} />
+              </Stack.Navigator>
+            </NavigationContainer>
           </>
         </QueryClientProvider>
       </ThemeProvider>
