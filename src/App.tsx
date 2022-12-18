1cfd3df137ef3ec546c27182c7da4b8b1c3906f6
Francesco <francesco.voto.8@gmail.com>
2022-12-18 18:09:32 +0100

    feat: add ThemeProvider to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import {SafeAreaView, StatusBar} from 'react-native';
 import {QueryClientProvider} from 'react-query';
 import queryClient from '@wst/services/queryClient';
+import theme, {ThemeProvider} from './theme';
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaView>
-        <StatusBar barStyle={'dark-content'} />
-      </SafeAreaView>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaView>
+          <StatusBar barStyle={'dark-content'} />
+        </SafeAreaView>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 };
 
