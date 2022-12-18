7fd4771ed554552f1b0e8028fc632659773b0440
Francesco <francesco.voto.8@gmail.com>
2022-12-18 20:55:01 +0100

    feat: add msw

---
+++ jest.setup.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+
+import {server} from './__mocks__/server';
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+
+jest.mock('react-native-safe-area-context', () => {
+  const inset = {top: 0, right: 0, bottom: 0, left: 0};
+  return {
+    SafeAreaProvider: jest.fn().mockImplementation(({children}) => children),
+    SafeAreaConsumer: jest
+      .fn()
+      .mockImplementation(({children}) => children(inset)),
+    useSafeAreaInsets: jest.fn().mockImplementation(() => inset),
+  };
+});
