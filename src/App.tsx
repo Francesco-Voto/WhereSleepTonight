af93b43bf0c331801994134ecd012db5cf0a3e06
Francesco <francesco.voto.8@gmail.com>
2022-12-18 18:30:32 +0100

    chore: add sare area context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import {SafeAreaView, StatusBar} from 'react-native';
 import {QueryClientProvider} from 'react-query';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import queryClient from '@wst/services/queryClient';
 import theme, {ThemeProvider} from './theme';
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <SafeAreaView>
-          <StatusBar barStyle={'dark-content'} />
-        </SafeAreaView>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <SafeAreaView>
+            <StatusBar barStyle={'dark-content'} />
+          </SafeAreaView>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 };
 
