6c4589dc05cd60806d8d7f149234858d8e28f648
Francesco <francesco.voto.8@gmail.com>
2022-12-18 22:02:46 +0100

    fix: setup correct import for testutils

--- __tests__/testUtils.tsx
+++ __tests__/testUtils.tsx
@@ -2,8 +2,8 @@ import {ReactElement} from 'react';
 import {render} from '@testing-library/react-native';
 import {QueryClientProvider} from 'react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import queryClient from '@wwt/services/queryClient';
-import theme, {ThemeProvider} from '@wwt/theme';
+import queryClient from '@wst/services/queryClient';
+import theme, {ThemeProvider} from '@wst/theme';
 
 export function renderWrapper(component: ReactElement) {
   return render(component, {
