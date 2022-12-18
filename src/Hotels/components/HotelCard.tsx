e625c44a587f72ca64c5bda825e715090c773455
Francesco <francesco.voto.8@gmail.com>
2022-12-18 21:52:49 +0100

    feat: create card to show hotel

---
+++ src/Hotels/components/HotelCard.tsx
@@ -0,0 +1,75 @@
+import {FC} from 'react';
+import {Image, Pressable, StyleSheet, View} from 'react-native';
+import Lottie from 'lottie-react-native';
+import type {Hotel} from '@wst/services/dataProvider';
+import {H1, H2, H3} from '@wst/components/Typography';
+import {Theme, useTheme} from '@wst/theme';
+import {CurrencyToSymbol} from '@wst/utils';
+const star = require('@wst/assets/star.json');
+
+interface Props {
+  hotel: Hotel;
+}
+
+const HotelCard: FC<Props> = ({hotel}) => {
+  const theme = useTheme();
+  const styles = generateStyles(theme);
+  return (
+    <Pressable style={styles.cardRoot}>
+      <Image
+        style={styles.image}
+        resizeMode="cover"
+        source={{
+          uri: hotel.gallery[0],
+        }}
+      />
+      <View style={styles.bodyContainer}>
+        <View style={styles.labelContainer}>
+          <H2>{hotel.name}</H2>
+          <View style={styles.starContainer}>
+            <Lottie source={star} autoPlay loop style={styles.star} />
+            <H3>{hotel.stars}</H3>
+          </View>
+        </View>
+        <H1 style={styles.price} variant="title">
+          {hotel.price}
+          <H2 variant="title">{CurrencyToSymbol[hotel.currency]}</H2>
+        </H1>
+      </View>
+    </Pressable>
+  );
+};
+
+const generateStyles = ({color, spacing}: Theme) =>
+  StyleSheet.create({
+    cardRoot: {
+      alignSelf: 'stretch',
+      backgroundColor: color.white,
+      overflow: 'hidden',
+      borderRadius: 8,
+    },
+    bodyContainer: {
+      padding: spacing.m,
+    },
+    labelContainer: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    image: {
+      height: 200,
+    },
+    star: {
+      height: 32,
+      width: 32,
+    },
+    starContainer: {
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    price: {
+      alignSelf: 'flex-end',
+    },
+  });
+
+export default HotelCard;
